b1f1be4256dea4772cd0185f36a80f3f33159166
Clemence Herny <clemence.herny@gmail.com>
2023-12-18 17:07:36 +0000

    Update header in scripts

--- scripts/detection_monitoring.py
+++ scripts/detection_monitoring.py
@@ -1,32 +1,8 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-#  Proj quarry detection and time machine
-#
-#      Clemence Herny 
-#      Shanci Li
-#      Alessandro Cerioni 
-#      Nils Hamel
-#      Huriel Reichel
-#      Copyright (c) 2020 Republic and Canton of Geneva
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# 
-################################################################
-#  Script used to plot and visualize quarry data trough times 
-#  Inputs are defined in config-dm.yaml
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
 
 
 import time

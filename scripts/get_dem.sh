1b6a6160baf236cd33c69350551804ba01ce53ff
Clemence Herny <clemence.herny@gmail.com>
2024-12-20 11:02:36 +0000

    Update path in get_dem.sh

--- scripts/get_dem.sh
+++ scripts/get_dem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download DEM tif file
 
-mkdir -p ./input/input_det/DEM/
-wget https://github.com/lukasmartinelli/swissdem/releases/download/v1.0/switzerland_dem.tif -O ./input/input_det/DEM/switzerland_dem.tif
-gdalwarp -t_srs "EPSG:2056" ./input/input_det/DEM/switzerland_dem.tif ./input/input_det/DEM/switzerland_dem_EPSG2056.tif
-rm ./input/input_det/DEM/switzerland_dem.tif
+mkdir -p ./data/DEM/
+wget https://github.com/lukasmartinelli/swissdem/releases/download/v1.0/switzerland_dem.tif -O ./data/DEM/switzerland_dem.tif
+gdalwarp -t_srs "EPSG:2056" ./data/DEM/switzerland_dem.tif ./data/DEM/switzerland_dem_EPSG2056.tif
+rm ./data/DEM/switzerland_dem.tif

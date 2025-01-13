40aa6fdb3649c9849e683dc12032e6e625af8d18
Clemence Herny <clemence.herny@gmail.com>
2025-01-13 19:04:27 +0000

    Add merge detection to batch file

--- scripts/batch_process.sh
+++ scripts/batch_process.sh
@@ -6,7 +6,7 @@
 
 echo 'Run batch processes to make quarries detection over several years'
 
-for year in YEAR1 YEAR2 YEAR3       # list of years to process (no comma: YEAR1 YEAR2 YEAR3 ...) 
+for year in YEAR1 YEAR2 YEAR3     # list of years to process (no comma: YEAR1 YEAR2 YEAR3 ...) 
 do
     echo ' '
     echo '-----------'
@@ -15,7 +15,7 @@ do
     sed -i "s/SWISSIMAGE_YEAR/$year/g" config/config_det_$year.yaml
     echo ' '
     echo 'prepare_data.py'
-    python3 ./scripts/prepare_data.py config/config_det_$year.yaml
+    python ./scripts/prepare_data.py config/config_det_$year.yaml
     echo ' '
     echo 'generate_tilesets.py'
     stdl-objdet generate_tilesets config/config_det_$year.yaml
@@ -23,6 +23,9 @@ do
     echo 'make_detections.py'
     stdl-objdet make_detections config/config_det_$year.yaml
     echo ' '
+    echo 'merge_detections.py'
+    python ./scripts/merge_detections.py config/config_det_$year.yaml
+    echo ' '
     echo 'filter_detections.py'
-    python3 ./scripts/filter_detections.py config/config_det_$year.yaml
+    python ./scripts/filter_detections.py config/config_det_$year.yaml
 done
